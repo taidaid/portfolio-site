1d6479cfa43b9545e572278c94edbfc07733ac29
Bryan <windsor.bryan@gmail.com>
2019-03-02 18:19:13 -0500

    populating About

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,90 @@
 import React from "react";
 
 const About = () => {
-  return <div className="App App-header">My name is Bryan Windsor. </div>;
+  return (
+    <div className="About">
+      您好, 我是翁博展.
+      <br />
+      My name is Bryan Windsor.
+      <div>
+        <h3>Software Developer</h3>
+        <p>
+          I first became interested in IT when I was running a Call of Duty clan
+          at the age of 15. As the clan leader, I had to setup and administer
+          the server and website which introduced me to networking and HTML/CSS
+        </p>
+      </div>
+      <div>
+        <h3>Polyglot</h3>
+        <p>
+          I first began learning languages when I was 12 years old and required
+          to take introductory courses in French and Spanish. I quickly
+          discovered that I had a knack for French. After 6 years of studying
+          French, I branched out into Mandarin Chinese and loved it. I studied
+          Chinese and French concurrently for 2 years in university before
+          traveling to China for a summer homestay program. I achieved limited
+          fluency during this program and tested out of all Mandarin courses
+          offered at my school. Sadly, this led to a decline in my ability.
+          After graduation and working in Healthcare IT for 3 years, I decided I
+          must revive this talent of mine and again enrolled in an intensive
+          Mandarin program, this time in Taiwan. After 4 years of study, travel,
+          and work in Asia, my Mandarin reached a comfotable level of
+          professional proficiency. I began a Chinese Language club in Utrecht,
+          Netherlands in order to share my passion with others. I have studied
+          Chinese, French, Dutch, German, and Spanish.
+        </p>
+      </div>
+      <div>
+        <h3>Adventurer</h3>
+        <p>
+          I learned to ski at 6 years old and never looked back. I have been
+          traveling, skiing, snowboarding, kitesurfing, wakeboarding, SCUBA
+          diving, cliff diving, free climbing, hiking, racing, exploring, and
+          loving every minute of it.
+        </p>
+        <ul>
+          <div>
+            <h5>Diver</h5>
+            <p>GA Aquarium, St. Thomas, Thailand, Fiji</p>
+          </div>
+          <div>
+            <h5>Skier</h5>
+            <p>Vale, Park City</p>
+          </div>
+          <div>
+            <h5>Climber</h5>
+            <p>Bouldering Halls</p>
+            <p>Waterfalls</p>
+          </div>
+        </ul>
+      </div>
+      <div>
+        <h3>Educator</h3>
+        <p>
+          During my time in Taiwan, I became an English teacher for
+          Chinese-speaking students. Through my Mandarin, I am able to easily
+          and efficiently explain complex concepts.
+        </p>
+      </div>
+      <div>
+        <h3>Healthcare IT Operations Manager</h3>
+        <p>
+          After graduation from university, I had the opportunity to work as an
+          IT Manager for 3 years in a healthcare management startup. In the
+          beginning, the company had one outsourced IT support specialist for
+          more than 50 employees and scattered, outdated, physical servers on
+          tape backups. By the end, I was truly working as a CIO, overseeing an
+          in-house team of 4 IT field technicians, a network engineer, and a
+          sysadmin. By utilizing an MSP for level 2 and 3 tickets that were
+          beyond our field techs' abilties, we effectively managed costs and
+          quality-of-service. With a dedicated "firefighting" team, the
+          sysadmin, network engineer, and myself were free to put our thoughts
+          on projects first and accomplish amazing feats, even though we were
+          "building the plane while flying".
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
