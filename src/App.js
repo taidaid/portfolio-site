cdc0dfcae2de7b82422a33d7078b5dcc3d421276
Bryan <windsor.bryan@gmail.com>
2019-03-06 19:03:13 -0500

    page transitions mostly working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import Home from "./components/Home";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
@@ -16,28 +17,34 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-        <div className="App">
-          <div className="navbar">
-            <Link to="/">
-              <img src={logo} className="App-logo" alt="logo" />
-            </Link>
-            <Link className="textLink" to="/about">
-              about
-            </Link>
-            <Link className="textLink" to="/portfolio">
-              portfolio
-            </Link>
-          </div>
-
-          <Switch>
-            <Route path="/about" component={About} />
-            <Route path="/portfolio" component={Portfolio} />
-            <Route exact path="/" component={Home} />
-          </Switch>
-        </div>
+        <Route
+          render={({ location }) => (
+            <TransitionGroup>
+              <CSSTransition key={location.key} classNames="fade" timeout={300}>
+                <div className="App">
+                  <div className="navbar">
+                    <Link to="/">
+                      <img src={logo} className="App-logo" alt="logo" />
+                    </Link>
+                    <Link className="textLink" to="/about">
+                      about
+                    </Link>
+                    <Link className="textLink" to="/portfolio">
+                      portfolio
+                    </Link>
+                  </div>
+                  <Switch location={location}>
+                    <Route path="/about" component={About} />
+                    <Route path="/portfolio" component={Portfolio} />
+                    <Route exact path="/" component={Home} />
+                  </Switch>
+                </div>
+              </CSSTransition>
+            </TransitionGroup>
+          )}
+        />
       </Router>
     );
-    // );
   }
 }
 
