de4fc1aa81241e243705efe1e5eee437719f75cf
Bryan <windsor.bryan@gmail.com>
2019-03-08 19:05:17 -0500

    adding portfolio pieces

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,10 +3,22 @@ import React from "react";
 const Portfolio = () => {
   return (
     <div className="Portfolio">
-      <div id="project-1">
+      <div className="project" id="project-1">
         <h3>Face Detection</h3>
         <a href="https://protected-taiga-22940.herokuapp.com/">
           <img src="https://i.imgur.com/NKhhycN.png" alt="Face Detection App" />
+        </a>
+      </div>
+      <div className="project" id="project-2">
+        <h3>Pixel Art</h3>
+        <a href="https://stormy-sands-35736.herokuapp.com/">
+          <img src="https://i.imgur.com/iPD9xtj.png" alt="Pixel Art App" />
+        </a>
+      </div>
+      <div className="project" id="project-3">
+        <h3>Bills Tracker</h3>
+        <a href="https://pacific-hollows-60748.herokuapp.com/">
+          <img src="https://i.imgur.com/Q8b3DKF.png" alt="Bills Tracker App" />
         </a>
       </div>
     </div>
