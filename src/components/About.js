4540f9cd5a584a20734fe2cd2622669c2dad363b
Bryan <windsor.bryan@gmail.com>
2019-03-08 19:28:02 -0500

    lorem

--- src/components/About.js
+++ src/components/About.js
@@ -26,7 +26,7 @@ const About = () => {
         <h3>Web Developer</h3>
         <div>
           <p>
-            I was introduced to programming in Java over 10 years ago as part of
+            {/* I was introduced to programming in Java over 10 years ago as part of
             my degree in Management Information Systems. Students with an MIS
             major study how companies and individuals can use systems and the
             generated data in decision-making processes. In other words, MIS
@@ -35,13 +35,18 @@ const About = () => {
             programming. In every group project, I quickly volunteered to do the
             back-end logic in Java. After graduation, my attention shifted to
             the technical aspects of IT Operations, system and network
-            administration, security, efficiency, and compliance.
+            administration, security, efficiency, and compliance. */}{" "}
+            Dolorum omnis veniam eum voluptas nam et distinctio. Culpa sed sequi
+            assumenda qui nam. Omnis veniam iure et voluptas autem ratione.
           </p>
           <p>
-            For over a year now, I have been fully focused on web development. A
+            {/* For over a year now, I have been fully focused on web development. A
             lot has changed in 10 years, but not the fundamentals of JS, HTML,
             and CSS. I love it more than ever as I can quickly build out single
-            page applications in React.js and
+            page applications in React.js and */}{" "}
+            Sunt id esse consequatur iusto reiciendis et ipsum autem dolor.
+            Repellendus reiciendis expedita modi saepe nam et numquam. Atque
+            officiis sit ut architecto sit et delectus.
           </p>
         </div>
       </div>
@@ -49,20 +54,33 @@ const About = () => {
         <h3>Hobbies</h3>
         <div>
           <p>
-            I have always had a curious mind and a hands-on attitude. When I was
+            Est provident perspiciatis non voluptate molestias omnis laudantium
+            nesciunt est. Saepe laudantium eum quos excepturi. Asperiores
+            numquam doloribus qui qui consequatur ratione. Nihil assumenda eaque
+            nemo officia.
+            {/* I have always had a curious mind and a hands-on attitude. When I was
             young, I helped my father build stairs, forts, and playgrounds. I
             landscaped our home and church, built homes for the needy, and
-            cooked in soup kitchens.
+            cooked in soup kitchens. */}
           </p>
           <p>
-            My first foray into history was the American Civil War, followed
+            Est provident perspiciatis non voluptate molestias omnis laudantium
+            nesciunt est. Saepe laudantium eum quos excepturi. Asperiores
+            numquam doloribus qui qui consequatur ratione. Nihil assumenda eaque
+            nemo officia.
+            {/* My first foray into history was the American Civil War, followed
             closely by the history of Rome. In recent years, I have focused on
             revolutions in particular, including French, English, American,
             South American, Haitian revolutions and now I am learning about the
-            political turmoil of 1848 in Europe.
+            political turmoil of 1848 in Europe. */}
           </p>
           <p>
-            I first began learning languages when I was 12 years old and
+            {" "}
+            Est provident perspiciatis non voluptate molestias omnis laudantium
+            nesciunt est. Saepe laudantium eum quos excepturi. Asperiores
+            numquam doloribus qui qui consequatur ratione. Nihil assumenda eaque
+            nemo officia.
+            {/* I first began learning languages when I was 12 years old and
             required to take introductory courses in French and Spanish. I
             quickly discovered that I had a knack for French. After 6 years of
             studying French, I branched out into Mandarin Chinese and loved it.
@@ -76,7 +94,7 @@ const About = () => {
             study, travel, and work in Asia, my Mandarin reached a comfotable
             level of professional proficiency. I began a Chinese Language club
             in Utrecht, Netherlands in order to share my passion with others. I
-            have studied Chinese, French, Dutch, German, and Spanish.
+            have studied Chinese, French, Dutch, German, and Spanish. */}
           </p>
         </div>
       </div>
@@ -84,10 +102,14 @@ const About = () => {
         <h3>Educator</h3>
         <div>
           <p>
-            During my time in Taiwan, I became an English teacher for
+            {/* During my time in Taiwan, I became an English teacher for
             Chinese-speaking students. Through my Mandarin, I am able to easily
             and efficiently explain complex concepts. This skill has earned me
-            hundreds of rave reviews from students and parents.
+            hundreds of rave reviews from students and parents. */}
+            Est provident perspiciatis non voluptate molestias omnis laudantium
+            nesciunt est. Saepe laudantium eum quos excepturi. Asperiores
+            numquam doloribus qui qui consequatur ratione. Nihil assumenda eaque
+            nemo officia.
           </p>
         </div>
       </div>
@@ -95,12 +117,16 @@ const About = () => {
         <h3>Adventurer</h3>
         <div>
           <p>
-            I learned to ski at 6 years old and never looked back. I have been
+            {/* I learned to ski at 6 years old and never looked back. I have been
             traveling, skiing, snowboarding, kitesurfing, wakeboarding, SCUBA
             diving, cliff diving, free climbing, hiking, racing, exploring, and
-            loving every minute of it.
+            loving every minute of it. */}
+            Est provident perspiciatis non voluptate molestias omnis laudantium
+            nesciunt est. Saepe laudantium eum quos excepturi. Asperiores
+            numquam doloribus qui qui consequatur ratione. Nihil assumenda eaque
+            nemo officia.
           </p>
-          <table>
+          {/* <table>
             <tbody>
               <tr>
                 <th>Countries Explored</th>
@@ -130,15 +156,15 @@ const About = () => {
                 </td>
               </tr>
             </tbody>
-          </table>
+          </table> */}
         </div>
       </div>
 
       <div className="about-block">
-        <h3>Healthcare IT Operations Manager</h3>
+        <h3>IT Operations Manager</h3>
         <div>
           <p>
-            After graduation from university, I had the opportunity to lead an
+            {/* After graduation from university, I had the opportunity to lead an
             IT department for 3 years in a healthcare management startup. In the
             beginning, the company had one outsourced IT support specialist for
             more than 50 employees and scattered, outdated, physical servers on
@@ -149,7 +175,11 @@ const About = () => {
             quality-of-service. With a dedicated "firefighting" team, the
             sysadmin, network engineer, and myself were free to put our thoughts
             on projects first and accomplish amazing feats, even though we were
-            "building the plane while flying".
+            "building the plane while flying". */}{" "}
+            Est provident perspiciatis non voluptate molestias omnis laudantium
+            nesciunt est. Saepe laudantium eum quos excepturi. Asperiores
+            numquam doloribus qui qui consequatur ratione. Nihil assumenda eaque
+            nemo officia.
           </p>
         </div>
       </div>
